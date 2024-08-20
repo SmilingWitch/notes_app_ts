0e1f0568f6039d00aeb382d8ef7dd32ae9b7943b
SmilingWitch <danielitayero@gmail.com>
2024-08-20 16:42:15 -0300

    feat: add StyledTextInput.tsx

---
+++ src/components/note_content/StyledTextInput.tsx
@@ -0,0 +1,104 @@
+import { TextInput, StyleSheet, StyleProp, TextStyle} from "react-native"
+import { useEffect, useRef } from "react";
+import lighTeme from "../../lightTheme";
+
+
+interface Props{
+    error?: string,
+    color?: string,
+    fontSize?: string,
+    fontWeight?: string,
+    header?: string,
+    style?: StyleProp<TextStyle>,
+    name?: string,
+    value?: string,
+    multiline?: boolean,
+    onChangeText?: (text: string) => void,
+    [x: string]: any;
+
+}
+
+
+const StyledTextInput = ({style = {}, error, color, fontSize, fontWeight, header , ...props}: Props) => {
+    
+    const inputRef = useRef<TextInput>(null);
+
+    useEffect(() => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      }, []);
+
+
+
+    const inputStyles = [
+        styles.textInput,
+        color === 'primary' && styles.colorPrimary,
+        color === 'secundary' && styles.colorSecundary,
+        fontSize === 'h1' && styles.h1,
+        fontSize === 'h2' && styles.h2,
+        fontSize === 'h3' && styles.h3,
+        fontSize === 'small' && styles.small,
+        fontWeight === 'bold' && styles.bold,
+        style,
+    ]
+    return(
+            <TextInput 
+                  style = {header ?  styles.notHeader: inputStyles } 
+                  {...props} 
+                  ref= {header && inputRef} 
+                  editable 
+                  multiline 
+                  inputMode="text"
+                  textAlignVertical="top"></TextInput>
+        
+    )
+}
+
+
+const styles = StyleSheet.create({
+    textInput: {
+        borderColor: lighTeme.colors.primary,
+        fontSize: lighTeme.fontSize.regular,
+        color: lighTeme.colors.textPrimary,
+     },
+     notHeader: {
+        borderColor: lighTeme.colors.primary,
+        fontSize: lighTeme.fontSize.regular,
+        color: lighTeme.colors.textPrimary,
+        flex: 1
+     },
+     error: {
+        borderColor: 'red'
+     },
+     colorPrimary: {
+        color: lighTeme.colors.textPrimary,
+        
+    },
+    colorSecundary: {
+        color: lighTeme.colors.primary
+    },
+    h1: {
+        fontSize: lighTeme.fontSize.h1
+    },
+    h2: {
+        fontSize: lighTeme.fontSize.h2
+    },
+    h3: {
+        fontSize: lighTeme.fontSize.h3
+    },
+    regular: {
+        fontSize: lighTeme.fontSize.regular,
+        fontWeight: lighTeme.fontWeight.normal,
+        color: lighTeme.colors.textPrimary
+    },
+    small: {
+        fontSize: lighTeme.fontSize.small
+    },
+    bold: {
+        fontWeight: lighTeme.fontWeight.bold
+    }
+})
+
+
+export default StyledTextInput
