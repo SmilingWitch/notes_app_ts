87c28d55b069e7a1286a129b8dd480d0138706eb
SmilingWitch <danielitayero@gmail.com>
2024-08-19 21:03:01 -0300

    feta: add NotesFoler.tsx

---
+++ src/screens/NotesFolder.tsx
@@ -0,0 +1,33 @@
+
+import { View, StyleSheet } from "react-native"
+import lighTeme from "../lightTheme"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { ParamListBase, RouteProp } from "@react-navigation/native"
+import NotesFolderHeader from "../components/notes_folder/NotesFolderHeader"
+import NotesFolderList from "../components/notes_folder/NotesFolderList"
+
+interface Props{
+    navigation: NativeStackNavigationProp<ParamListBase>;
+}
+
+
+const NotesFolder = ({navigation}: Props) => {
+    return(
+        <View style = {styles.container}>
+            <NotesFolderHeader navigation = {navigation} />
+            <NotesFolderList navigation = {navigation} touch = {true}/>
+        </View>
+        
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: lighTeme.colors.primary
+    }
+    
+})
+
+export default NotesFolder
