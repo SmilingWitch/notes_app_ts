fe8d3f2dae096594e0cc718d5121c0055f5dad1e
SmilingWitch <danielitayero@gmail.com>
2024-08-21 18:31:24 -0300

    feat: add redux provider to App.tsx

--- App.tsx
+++ App.tsx
@@ -2,13 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/components/Main';
 import lighTeme from './src/lightTheme';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/store/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main/>
-      <StatusBar style="light" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <Main/>
+          <StatusBar style="light" />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 }
 
