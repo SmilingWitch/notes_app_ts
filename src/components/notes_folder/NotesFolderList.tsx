ddcf815076b4a25260f06c9e5567dcc31b277891
SmilingWitch <danielitayero@gmail.com>
2024-08-26 19:35:07 -0300

    feat: add id prop to rederItems in NotesFolderList

--- src/components/notes_folder/NotesFolderList.tsx
+++ src/components/notes_folder/NotesFolderList.tsx
@@ -5,14 +5,15 @@ import lighTeme from "../../lightTheme"
 import { NotesFolderListProps } from "../../types"
 import { useSelector } from "react-redux"
 
-const NotesFolderList = ({navigation, touch}: NotesFolderListProps) => {
+const NotesFolderList = ({navigation, touch, setShowThrash}: NotesFolderListProps) => {
     
     const folders = useSelector((state: any) => state.folder)
-    console.log(folders)
 
+    console.log(folders)
+    
     const renderItem = ({item}: any) => (
-        touch === true ? <NotesFolderItem name = {item.name} amount = {3} navigation = {navigation}/>:
-                          <NotesFolderItemWithoutTouch name = {item.name} amount = {3}/>
+        touch === true ? <NotesFolderItem name = {item.name} amount = {3} navigation = {navigation} id = {item.id}/>:
+                          <NotesFolderItemWithoutTouch name = {item.name} amount = {3} id = {item.id} setShowThrash = {setShowThrash}/>
     )
 
 
