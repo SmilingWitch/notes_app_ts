74798451f92d2ea09f2faf9e1966815e413a11a0
SmilingWitch <danielitayero@gmail.com>
2024-08-26 19:27:59 -0300

    ref: delete data = [] from ScrollCategories

--- src/components/notes/ScrollCategories.tsx
+++ src/components/notes/ScrollCategories.tsx
@@ -4,40 +4,13 @@ import CategoryItem from "./CategoryItem";
 import { AppBarProps } from "../../types";
 import { useSelector } from "react-redux";
 
-/*const data = [
-    {
-        id: 1,
-        name: 'All',
-        amount: 100
-    },
-    {
-        id: 2,
-        name: 'Novel',
-        amount: 20
-    },
-    {
-        id: 3,
-        name: 'Lists',
-        amount: 5
-    },
-    {
-        id: 4,
-        name: 'Exercise',
-        amount: 10
-    },
-    {
-        id: 5,
-        name: 'Books',
-        amount: 3
-    }
-]*/
+
 
 const ScrollCategories = ({route ,navigation}: AppBarProps) => {
 
     const { category_name } = route.params;
     const folders = useSelector((state: any) => state.folder)
-    console.log(category_name)
-
+    
     const renderItem = ({item}: any) => (
         <>
         <CategoryItem   name = {item.name} 
