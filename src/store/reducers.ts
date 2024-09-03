87e598f5a6fd38c01068c0dc9f9c389a62a703d6
SmilingWitch <danielitayero@gmail.com>
2024-09-02 22:02:26 -0300

    feat: add  clearSelectedFolderById, clearSelectedNote, clearSelectedNoteById reducers

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -16,20 +16,21 @@ export interface UserState {
   notes: Note[];
   folder: Folder[],
   selectedFolderID: any[],
-  selectedNoteID: number
+  selectedNoteID: any[]
 }
 
 const initialState: UserState = {
   notes: [],
   folder: [],
   selectedFolderID: [],
-  selectedNoteID: 0
+  selectedNoteID: []
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+
     createFolder: (state, action) => {
       const newFolder: Folder = {
         id: Date.now(), // Puedes usar un generador de ID más robusto
@@ -37,6 +38,7 @@ const userSlice = createSlice({
       };
       state.folder.push(newFolder);
     },
+
     deleteFolder: (state, action) => {
       const name = action.payload
       // Itera sobre cada carpeta en foldersToDelete para eliminarlas junto con sus notas asociadas
@@ -48,24 +50,38 @@ const userSlice = createSlice({
 
       // Elimina las notas cuya categoría coincide con el nombre de la carpeta eliminada
       state.notes = state.notes.filter((note: any) => note.category !== folderToDelete.name);
-  });
+    });
       
       state.selectedFolderID = []
     },
+
     selectedFolder: (state, action) => {
       const folderId = action.payload;
       state.selectedFolderID.push(folderId);
     },
+
     clearSelectedFolder: (state, action) => {
       state.selectedFolderID = []
     },
+
     clearSelectedFolderById: (state, action) => {
       const folderId = action.payload
       state.selectedFolderID = state.selectedFolderID.filter((folder: any) => folder.id !== folderId)
     },
+
     selectedNote: (state, action) => {
       const noteId = action.payload;
-      state.selectedNoteID = noteId;
+      state.selectedNoteID.push(noteId);
+      /*state.selectedNoteID = []*/
+    },
+
+    clearSelectedNote: (state, action) => {
+      state.selectedNoteID = []
+    },
+
+    clearSelectedNoteById: (state, action) => {
+      const noteId = action.payload
+      state.selectedNoteID = state.selectedNoteID.filter((note: any) => note.id !== noteId)
     },
     
     addNote: (state, action) => {
@@ -78,6 +94,7 @@ const userSlice = createSlice({
       };
       state.notes.push(newNote);
     },
+
     updateNote: (state, action) => {
       const { id, title, content } = action.payload;
       const noteIndex = state.notes.findIndex((note) => note.id === id);
@@ -85,11 +102,24 @@ const userSlice = createSlice({
         state.notes[noteIndex] = { ...state.notes[noteIndex], title, content };
       }
     },
+
     deleteNote: (state, action) => {
-      const noteId = action.payload;
-      console.log("NOTE ID",noteId)
+
       state.notes = state.notes.filter((note) => note.id !== noteId);
+      const noteId = action.payload
+
+      console.log("NOTE ID", noteId)
+      // Itera sobre cada carpeta en foldersToDelete para eliminarlas junto con sus notas asociadas
+      noteId.forEach((noteToDelete: { id: number }) => {
+
+      // Elimina la carpeta cuyo ID coincide con el ID del objeto en foldersToDelete
+      state.notes = state.notes.filter((note: any) => note.id !== noteToDelete.id);
+
+    });
+      
+    state.selectedNoteID = []
     },
+
   },
 });
 
@@ -101,5 +131,7 @@ export const {  addNote,
                 selectedFolder, 
                 selectedNote,
                 clearSelectedFolder,
-                clearSelectedFolderById } = userSlice.actions;
+                clearSelectedFolderById,
+                clearSelectedNote,
+                clearSelectedNoteById } = userSlice.actions;
 export default userSlice.reducer;
