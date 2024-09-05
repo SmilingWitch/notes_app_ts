15ed8c0958bca4c0ff6f97b60beef68ea3d952c9
SmilingWitch <danielitayero@gmail.com>
2024-09-05 20:45:50 -0300

    feat: add darkTheme.ts

---
+++ src/darkTheme.ts
@@ -0,0 +1,28 @@
+
+const lighTeme = {
+    colors: {
+        textPrimary: '#fefefe',
+        textSecundary: '#586069',
+        primary: '#1D1E20',
+        secundary: '#209ff0',
+        white: "#fefefe",
+        light: '80A0B5',
+        grey: '#272727',
+        lightGrey: '#D2D2D2'
+    },
+    fontSize: {
+        h1: 32,
+        h2: 24,
+        h3: 18.72,
+        regular: 16,
+        small: 12
+    },
+    fonts: 'System',
+    fontWeight: {
+        normal: '400',
+        bold: '700'
+    },
+    padding: 10
+}
+
+export default lighTeme
