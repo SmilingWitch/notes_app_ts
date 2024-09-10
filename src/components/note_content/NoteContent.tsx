88dbdd933a52de9e0876e45c47efedf08836ac7f
SmilingWitch <danielitayero@gmail.com>
2024-09-09 22:08:39 -0300

    feat: add formatedBar to NoteContent

--- src/components/note_content/NoteContent.tsx
+++ src/components/note_content/NoteContent.tsx
@@ -1,8 +1,9 @@
-import { StyleSheet, ScrollView, KeyboardAvoidingView} from "react-native"
-import { useRef} from "react"
+import { StyleSheet, ScrollView, KeyboardAvoidingView, Text, View} from "react-native"
+import { useEffect, useRef, useState} from "react"
 import StyledTextInput from "./StyledTextInput"
 import lighTeme from "../../lightTheme"
 import { NoteContentProps } from "../../types"
+import FormatingButton from "./FormatingButton"
 
 
 
@@ -12,104 +13,140 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
       const handleFocus = () => {
             inputRef.current?.focus(); // Muestra el teclado
         };
-        
 
-        /*//Select fraction of text
+      
+        
+        const [input, setinput] = useState(content);
+        const [formatedText, setFormatedText] = useState(content);
+    
+        //Select fraction of text
         const [selection, setSelection] = useState({ start: 0, end: 0 });
 
-        // Función para encontrar los límites de una palabra
-        const findWordBoundary = (text: string, position: number): [number, number] => {
-            let start = position;
-            let end = position;
-
-            // Busca hacia atrás para encontrar el inicio de la palabra
-            while (start > 0 && text[start - 1] !== " " && text[start - 1] !== "\n") {
-              start--;
-            }
-        
-            // Busca hacia adelante para encontrar el final de la palabra
-            while (end < text.length && text[end] !== " " && text[end] !== "\n") {
-              end++;
-            }
-        
-            return [start, end];
-        };
-
-        const formattedWordFnc = (sufix: string, formattedWord: string) => {
-           console.log("SELECTED WORD",formattedWord )
-           if( formattedWord.startsWith(sufix) && formattedWord.endsWith(sufix) ){
-               formattedWord = formattedWord.slice(2, -2)
-           }else{
-                formattedWord = `${sufix}${formattedWord}${sufix}`
-                console.log("SUFIX",formattedWord)
-           }
-
-           return formattedWord
-         }
-
-        const handleFormat = (style: any) => {
-          const start = selection.start;
-          const end = selection.end;
-        
-          if(start !== end) {
-              // Encuentra el inicio y el fin de la palabra seleccionada
-          const [wordStart, wordEnd] = findWordBoundary(input, start);
-            
-          // Extrae la palabra seleccionada
-          const selectedWord = input.substring(wordStart, wordEnd);
-            
-          // Aplica el formato a la palabra seleccionada
-          let formattedWord = selectedWord;
-            
-          switch (style) {
-            case "bold":
-              formattedWord = formattedWordFnc("**", formattedWord)
-
-              break;
-            case "italic":
-              formattedWord = formattedWordFnc("//", formattedWord)
-              break;
-            case "underline":
-              formattedWord = formattedWordFnc("-", formattedWord)
-              break;
-            default:
-              break;
-          }
-      
-          // Reemplaza la palabra seleccionada en el texto con la versión formateada
-          setInput(input?.replace(selectedWord, formattedWord));
-      
-          }else{
-              console.log("No se ha seleccionado ninguna palabra")
-          }
-        };
-
+        useEffect(() => {
+         setFormatedText(applyStyles(input)) 
+        }, [])
     
+        // Función para encontrar los límites de una palabra
+      const findWordBoundary = (text: string, position: number): [number, number] => {
+        let start = position;
+        let end = position;
+    
+        // Busca hacia atrás para encontrar el inicio de la palabra
+        while (start > 0 && text[start - 1] !== " " && text[start - 1] !== "\n") {
+          start--;
+        }
+    
+        // Busca hacia adelante para encontrar el final de la palabra
+        while (end < text.length && text[end] !== " " && text[end] !== "\n") {
+          end++;
+        }
+    
+        return [start, end];
+      };
+    
+     const formattedWordFnc = (sufix: string, formattedWord: string) => {
+        console.log("SELECTED WORD",formattedWord )
+            if( formattedWord.startsWith(sufix) && formattedWord.endsWith(sufix) ){
+                formattedWord = formattedWord.slice(2, -2)
+            }else{
+                 formattedWord = `${sufix}${formattedWord}${sufix}`
+                 console.log("SUFIX",formattedWord)
+            }
+            
+            return formattedWord
+      }
+    
+      const handleFormat = (style: any) => {
+        const start = selection.start;
+        const end = selection.end;
 
-      console.log(selection)
+        if (start === end) {
+          // Si no hay texto seleccionado, no aplicar formato.
+          console.log("No hay texto seleccionado");
+          return;
+      }
+      
+       // Extrae el texto seleccionado
+    const selectedText = input.substring(start, end);
+    
+    // Aplica el formato al texto seleccionado
+    let formattedText = selectedText;
+    
+        // Encuentra el inicio y el fin de la palabra seleccionada
+        const [wordStart, wordEnd] = findWordBoundary(input, start);
+    
+        // Extrae la palabra seleccionada
+        const selectedWord = input.substring(wordStart, wordEnd);
+    
+        // Aplica el formato a la palabra seleccionada
+        let formattedWord = selectedWord;
+    
+        switch (style) {
+          case "bold":
+            formattedWord = formattedWordFnc("**", formattedWord)
+            break;
+          case "italic":
+            formattedWord = formattedWordFnc("//", formattedWord)
+            break;
+          case "underline":
+            formattedWord = formattedWordFnc("-", formattedWord)
+            break;
+          default:
+            break;
+        }
+    
+        const newText = input.substring(0, start) + formattedText + input.substring(end);
 
-      const applyStyles = (text: any) => {
-        const parts = text.split(" "); // Divide el texto por las etiquetas de estilo
-        const partsWithSignals = text.split(" ")
 
-        console.log("partsWithSignals", partsWithSignals)
-        console.log("parts", parts)
+        // Reemplaza la palabra seleccionada en el texto con la versión formateada
+        setinput(input.replace(selectedWord, formattedWord));
+        const formatedText = applyStyles(input)
+        console.log("FORMATED TEXT", formatedText)
+        setFormatedText(formatedText)
 
-        console.log("parts",parts)
-        return parts.map((part: string, index: number) => {
-          if (part.startsWith('**') && part.endsWith('**') ) {
-            console.log("PART",part)
-            return <Text key={index} style={{ fontWeight: 800 }}>{part.slice(2, -2)}{" "}</Text>;
-          } else if (part.startsWith('//') && part.endsWith('//')) {
-            return <Text key={index} style={{ fontStyle: 'italic' }}>{part.slice(1, -1)}</Text>;
-          } else if (part.startsWith('-') && part.endsWith('-')) {
-            return <Text key={index} style={{ backgroundColor: 'grey' }}>{part.slice(3, -4)}</Text>;
-          } else {
-            console.log(part)
-            return part + " ";
-          }
-        });
-      };*/
+        
+        
+        return formatedText
+      };
+
+      
+    
+        
+    
+          console.log(selection)
+    
+          const applyStyles = (text: any) => {
+            const parts = text.split(" "); // Divide el texto por las etiquetas de estilo
+            const partsWithSignals = text.split(" ")
+    
+            console.log("partsWithSignals", partsWithSignals)
+            console.log("parts", parts)
+    
+            console.log("parts",parts)
+            return parts.map((part: string, index: number) => {
+              if (part.startsWith('**') && part.endsWith('**') ) {
+                console.log(part)
+                return <Text key={index} style={{ fontWeight: 800 }}>{part.slice(2, -2)}{" "}</Text>;
+              } else if (part.startsWith('//') && part.endsWith('//')) {
+                return <Text key={index} style={{ fontStyle: 'italic' }}>{part.slice(1, -1)}</Text>;
+              } else if (part.startsWith('-') && part.endsWith('-')) {
+                return <Text key={index} style={{ backgroundColor: 'grey' }}>{part.slice(3, -4)}</Text>;
+              } else {
+                console.log(part)
+                return part + " " ;
+                
+              }
+              
+            });
+          };
+
+      
+          const handleTextChange = (text: string) => {
+            setInput(text); // Actualiza el primer estado
+            setinput(text); // Actualiza el segundo estado
+        };
+    
+         
 
       
 
@@ -123,44 +160,44 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
                         style={styles.input}
                         name='content'
                         multiline
-                        onChangeText={setInput}
-                        /*onSelectionChange={({ nativeEvent: { selection } }: any) => {setSelection(selection), console.log(selection)}}
-                        selection={selection} */
+                        onChangeText={handleTextChange}
+                        onSelectionChange={({ nativeEvent: { selection } }: any) => {setSelection(selection), console.log(selection)}}
+                        selection={selection} 
                         header = 'header'
                         selectedTextStyle
                         autoFocus={true} // Enfoca el input al renderizar
                         onFocus={handleFocus}
-                    >{/*applyStyles(input)*/ content}</StyledTextInput>
+                    > {formatedText}</StyledTextInput>
                     
                 </ScrollView>
                 
 
 
-        {/*<View>
+        <View>
               <ScrollView horizontal style = {styles.scroll_container}  keyboardShouldPersistTaps="always">
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'bold' onPress={() => handleFormat('bold')}/>
+                  <FormatingButton name = 'bold' onPress={() => {handleFormat('bold') }}/>
                   <FormatingButton name = 'italic' onPress={() => handleFormat('italic')}/>
-                  <FormatingButton name = 'circle' onPress={() => handleFormat('resalt')}/>
-                  <FormatingButton name = 'text-height' onPress={() => handleFormat('height')}/>
+                  <FormatingButton name = 'circle' /*onPress={() => handleFormat('resalt')}*//>
+                  <FormatingButton name = 'text-height' /*onPress={() => handleFormat('height')}*//>
               </View>
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'check-square-o' onPress={() => handleFormat('to-do-list')}> </FormatingButton>
-                  <FormatingButton name = 'list-ul' onPress={() => handleFormat('list-binetas')}/>
-                  <FormatingButton name = 'list-ol' onPress={() => handleFormat('enum-list')}/>
+                  <FormatingButton name = 'check-square-o' /*onPress={() => handleFormat('to-do-list')}*/> </FormatingButton>
+                  <FormatingButton name = 'list-ul' /*onPress={() => handleFormat('list-binetas')}*//>
+                  <FormatingButton name = 'list-ol' /*onPress={() => handleFormat('enum-list')}*//>
 
               </View>
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'align-justify' onPress={() => handleFormat('justify')}/>
-                  <FormatingButton name = 'align-left' onPress={() => handleFormat('aling-left')}/>
-                  <FormatingButton name = 'align-right' onPress={() => handleFormat('aling-right')}/>
+                  <FormatingButton name = 'align-justify' /*onPress={() => handleFormat('justify')}*//>
+                  <FormatingButton name = 'align-left' /*onPress={() => handleFormat('aling-left')}*//>
+                  <FormatingButton name = 'align-right' /*onPress={() => handleFormat('aling-right')}*//>
               </View>
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'rotate-right' onPress={() => handleFormat('back')}/>
-                  <FormatingButton name = 'rotate-left' onPress={() => handleFormat('behind')}/>
+                  <FormatingButton name = 'rotate-right' /*onPress={() => handleFormat('back')}*//>
+                  <FormatingButton name = 'rotate-left' /*onPress={() => handleFormat('behind')}*//>
               </View>
           </ScrollView>
-          </View>*/}  
+          </View>
             </KeyboardAvoidingView>
 
     )
