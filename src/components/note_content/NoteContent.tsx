042692d7e031cdb6d066832293d6616ea28ef423
SmilingWitch <danielitayero@gmail.com>
2024-09-11 16:02:52 -0300

    feat: save index and apply styles in NoteContent

--- src/components/note_content/NoteContent.tsx
+++ src/components/note_content/NoteContent.tsx
@@ -4,6 +4,9 @@ import StyledTextInput from "./StyledTextInput"
 import lighTeme from "../../lightTheme"
 import { NoteContentProps } from "../../types"
 import FormatingButton from "./FormatingButton"
+import { useDispatch, useSelector } from "react-redux"
+import { addNotesStyle } from "../../store/reducers"
+import StyledText from "../common/StyledText"
 
 
 
@@ -13,142 +16,70 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
       const handleFocus = () => {
             inputRef.current?.focus(); // Muestra el teclado
         };
-
-      
         
         const [input, setinput] = useState(content);
-        const [formatedText, setFormatedText] = useState(content);
     
         //Select fraction of text
         const [selection, setSelection] = useState({ start: 0, end: 0 });
-
-        useEffect(() => {
-         setFormatedText(applyStyles(input)) 
-        }, [])
-    
-        // Función para encontrar los límites de una palabra
-      const findWordBoundary = (text: string, position: number): [number, number] => {
-        let start = position;
-        let end = position;
-    
-        // Busca hacia atrás para encontrar el inicio de la palabra
-        while (start > 0 && text[start - 1] !== " " && text[start - 1] !== "\n") {
-          start--;
-        }
-    
-        // Busca hacia adelante para encontrar el final de la palabra
-        while (end < text.length && text[end] !== " " && text[end] !== "\n") {
-          end++;
-        }
-    
-        return [start, end];
-      };
-    
-     const formattedWordFnc = (sufix: string, formattedWord: string) => {
-        console.log("SELECTED WORD",formattedWord )
-            if( formattedWord.startsWith(sufix) && formattedWord.endsWith(sufix) ){
-                formattedWord = formattedWord.slice(2, -2)
-            }else{
-                 formattedWord = `${sufix}${formattedWord}${sufix}`
-                 console.log("SUFIX",formattedWord)
-            }
-            
-            return formattedWord
-      }
-    
-      const handleFormat = (style: any) => {
-        const start = selection.start;
-        const end = selection.end;
-
-        if (start === end) {
-          // Si no hay texto seleccionado, no aplicar formato.
-          console.log("No hay texto seleccionado");
-          return;
-      }
-      
-       // Extrae el texto seleccionado
-    const selectedText = input.substring(start, end);
-    
-    // Aplica el formato al texto seleccionado
-    let formattedText = selectedText;
-    
-        // Encuentra el inicio y el fin de la palabra seleccionada
-        const [wordStart, wordEnd] = findWordBoundary(input, start);
-    
-        // Extrae la palabra seleccionada
-        const selectedWord = input.substring(wordStart, wordEnd);
-    
-        // Aplica el formato a la palabra seleccionada
-        let formattedWord = selectedWord;
-    
-        switch (style) {
-          case "bold":
-            formattedWord = formattedWordFnc("**", formattedWord)
-            break;
-          case "italic":
-            formattedWord = formattedWordFnc("//", formattedWord)
-            break;
-          case "underline":
-            formattedWord = formattedWordFnc("-", formattedWord)
-            break;
-          default:
-            break;
-        }
-    
-        const newText = input.substring(0, start) + formattedText + input.substring(end);
-
-
-        // Reemplaza la palabra seleccionada en el texto con la versión formateada
-        setinput(input.replace(selectedWord, formattedWord));
-        const formatedText = applyStyles(input)
-        console.log("FORMATED TEXT", formatedText)
-        setFormatedText(formatedText)
-
+        const [formattedText, setFormattedText] = useState("")
         
-        
-        return formatedText
-      };
-
-      
-    
-        
-    
-          console.log(selection)
-    
-          const applyStyles = (text: any) => {
-            const parts = text.split(" "); // Divide el texto por las etiquetas de estilo
-            const partsWithSignals = text.split(" ")
-    
-            console.log("partsWithSignals", partsWithSignals)
-            console.log("parts", parts)
-    
-            console.log("parts",parts)
-            return parts.map((part: string, index: number) => {
-              if (part.startsWith('**') && part.endsWith('**') ) {
-                console.log(part)
-                return <Text key={index} style={{ fontWeight: 800 }}>{part.slice(2, -2)}{" "}</Text>;
-              } else if (part.startsWith('//') && part.endsWith('//')) {
-                return <Text key={index} style={{ fontStyle: 'italic' }}>{part.slice(1, -1)}</Text>;
-              } else if (part.startsWith('-') && part.endsWith('-')) {
-                return <Text key={index} style={{ backgroundColor: 'grey' }}>{part.slice(3, -4)}</Text>;
-              } else {
-                console.log(part)
-                return part + " " ;
-                
-              }
-              
-            });
-          };
-
-      
           const handleTextChange = (text: string) => {
             setInput(text); // Actualiza el primer estado
             setinput(text); // Actualiza el segundo estado
         };
-    
-         
+        const dispatch = useDispatch()
+        const noteStyles = useSelector((state: any) => state.noteStyles)
 
-      
+        useEffect(() => {
+          const format = applyStyles(noteStyles,content)
+          setFormattedText(format)
+          console.log("SE ELECUTO")
+
+        }, [])
+
+        
+
+        console.log(noteStyles.note_id.bold)
+
+        const createRange =  (selected: any, style: string) =>{
+          if(selected.start !== selected.end){
+            dispatch(addNotesStyle({style: style, selected: selected, noteStyles: noteStyles}))
+            const format = applyStyles(noteStyles, content)
+            setFormattedText(format)
+          }
+        }
+
+        const getSelectedText = (text: string, selection: { start: number; end: number }) => {
+          const beforeText = text.substring(0, selection.start - 1);
+          const selectedText = text.substring(selection.start - 1, selection.end);
+          const afterText = text.substring(selection.end);
+
+          
+
+          return {beforeText, selectedText, afterText};
+      };
+
+        const applyStyles = (noteStyles: any, text: any) => {
+
+          const {beforeText,selectedText, afterText} = getSelectedText(text, selection)
+          
+            if(noteStyles.note_id.bold){
+              return(<StyledText>
+                      <StyledText >{beforeText}</StyledText>
+                      <StyledText  fontWeight="bold">{selectedText}</StyledText>  
+                      <StyledText >{afterText}</StyledText>
+                    </StyledText>
+              
+              )
+              
+            }else{
+              return <StyledText>{content} </StyledText>;
+            }
+
+        }
+ 
+
+      console.log("FORMATTED",formattedText)
 
     return(
             <KeyboardAvoidingView
@@ -167,7 +98,7 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
                         selectedTextStyle
                         autoFocus={true} // Enfoca el input al renderizar
                         onFocus={handleFocus}
-                    > {formatedText}</StyledTextInput>
+                    > {formattedText}</StyledTextInput>
                     
                 </ScrollView>
                 
@@ -176,8 +107,10 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
         <View>
               <ScrollView horizontal style = {styles.scroll_container}  keyboardShouldPersistTaps="always">
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'bold' onPress={() => {handleFormat('bold') }}/>
-                  <FormatingButton name = 'italic' onPress={() => handleFormat('italic')}/>
+                  <FormatingButton name = 'bold' onPress={() => {createRange(selection, "bold")
+                    applyStyles(noteStyles, content)
+                  }}/>
+                  <FormatingButton name = 'italic' onPress={() => createRange(selection, "italic")}/>
                   <FormatingButton name = 'circle' /*onPress={() => handleFormat('resalt')}*//>
                   <FormatingButton name = 'text-height' /*onPress={() => handleFormat('height')}*//>
               </View>
