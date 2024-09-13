4b60a7d51da28578cb83051568e2cebf5b7dd6fb
SmilingWitch <danielitayero@gmail.com>
2024-09-13 15:53:17 -0300

    ref: fix addNotesStyle in reducers.ts

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -110,44 +110,13 @@ const userSlice = createSlice({
         1725927911731: {
           bold: [{
             start: 0,
-            end: 2,
+            end: 23,
           },]
         },
       }*/
       
       const isEmpty = (obj: any) => Object.keys(obj).length === 0; // Función para comprobar si un objeto está vacío
-    
-     /* // Si no hay estilos en la nota, agregar el nuevo estilo directamente
-      if (isEmpty(noteStyles)) {
-        state.noteStyles = newRangeObject; // Insertamos el objeto con el nuevo rango
-      } else {
-        // Si ya hay estilos para la nota, verificar si el estilo actual existe (e.g., 'bold')
-        if (noteStyles[noteId] && noteStyles[noteId][style]) {
-          // Verificar si el rango ya existe
-          const rangeExists = noteStyles[noteId][style].some((range: { start: number; end: number }) => {
-            return range.start === selected.start && range.end === selected.end;
-          });
-    
-          // Si el rango ya existe, lo eliminamos
-          if (rangeExists) {
-            noteStyles[noteId][style] = noteStyles[noteId][style].filter((range: { start: number; end: number }) => {
-              return !(range.start === selected.start && range.end === selected.end);
-            });
-            console.log('El rango existente ha sido eliminado');
-          } 
-          else {
-            // Si no existe, agregamos el nuevo rango
-            noteStyles[noteId][style].push({ start: selected.start, end: selected.end });
-            console.log('Nuevo rango agregado');
-          }
-        } else {
-          // Si el estilo no existe aún, creamos el array con el nuevo rango
-          noteStyles[noteId] = {
-            ...noteStyles[noteId],
-            [style]: [{ start: selected.start, end: selected.end }]
-          };
-        }
-      }*/
+  
       console.log("NOTE STYLES",noteStyles[noteId].bold)
       
 
@@ -164,20 +133,13 @@ const userSlice = createSlice({
 
             // Si el rango seleccionado esta contenido dentro de algun rango
             if(range.start <= selected.start && range.end >= selected.end){
-              
-                
-
-            console.log("El rango esta contenido dentro de otro rango")
-
-                
-
+              console.log("El rango esta contenido dentro de otro rango")  
               newRanges.push({ start: range.start , end: selected.start - 1 }); // Rango anterior
               newRanges.push({ start: selected.end , end: range.end  }); // Rango posterior
-
               console.log("NEW RANGES", newRanges)
               
 
-              /*// Elimino el rango grande 
+              // Elimino el rango grande 
               noteStyles[noteId][style] = noteStyles[noteId][style].filter(item => {
                 console.log("item.start", item.start);
                 console.log("range.start", range.start);
@@ -192,27 +154,23 @@ const userSlice = createSlice({
 
               console.log("NEW NOTES STYLES",noteStyles[noteId][style])
 
-              newRanges = []*/
+              newRanges = []
 
               
 
             } 
-          })
-
-        }
           
-      } 
+           
 
-                
-      if (range.start === selected.start && range.end === selected.end ) {
+            // si el rango existe
+       else if (range.start === selected.start && range.end === selected.end ) {
         console.log("El rango existe ")
-        console.log("selected.start", selected.start);
-        console.log("range.start", range.start);
-        console.log("selected.end", selected.end );
-        console.log("range.end", range.end );
+        
 
         noteStyles[noteId][style] = noteStyles[noteId][style].filter((range: { start: number; end: number }) => {
-          return !(range.start === selected.start && range.end   === selected.end - 1);
+          return !(range.start === selected.start && range.end   === selected.end - 1
+
+          );
         }); 
         console.log("NOTEEEEEE", noteStyles[noteId][style])
         
@@ -221,30 +179,42 @@ const userSlice = createSlice({
         console.log("Hay solapamiento")
 
                 
-      }
-      else {
+      } else {
+        console.log("AGREGADO AQUI--------------------------------------------------------------------------")
         let rangeAdd = []
           // Si no hay solapamiento, simplemente añade el rango existente
           console.log("NO hay solapamiento", selected)
           rangeAdd.push({end: selected.end - 1 , start: selected.start })
           console.log("Se agrego un rango",rangeAdd)
 
-          /*noteStyles[noteId][style].push({end: selected.end - 1 , start: selected.start });
-          console.log(noteStyles[noteId][style])
+          noteStyles[noteId][style].push({end: selected.end - 1 , start: selected.start });
+          console.log("NOTE STYLES MAS AGREGADO",noteStyles[noteId][style])
 
           const noteStylesCopy = [...noteStyles[noteId][style]];
 
+          console.log("NOTE STYLES COPIA", noteStylesCopy)
+
           // Ordena la copia
           const noteSort = noteStylesCopy.sort((a, b) => a.start - b.start);
-        
           console.log("NOTE SORT", noteStyles[noteId][style]);
         
           // Si necesitas actualizar el estado con el array ordenado, asigna la copia ordenada al estado
-          state.noteStyles[noteId][style] = noteSort;*/
-
-          
-          
+          state.noteStyles[noteId][style] = noteSort;
         }
+          })
+        } else {
+          // Si el estilo no existe aún, creamos el array con el nuevo rango
+          noteStyles[noteId] = {
+            ...noteStyles[noteId],
+            [style]: [{ start: selected.start, end: selected.end }],
+          };
+          console.log('Nuevo estilo creado con el rango', noteStyles[noteId][style]);
+        }
+      
+          
+      }
+
+      
       
 
 
@@ -315,22 +285,6 @@ const userSlice = createSlice({
       }*/
       console.log("--------------------------------------------------------------------------")
     },
-
-    
-    
-        /*state.noteStyles = {
-          note_id: {
-            bold: [{
-              start: 0,
-              end: 0,
-            },]
-          },
-        }
-      console.log("NOTE STYLES",noteStyles.note_id.bold) */
-    
-    
-    
-
    
     
     clearSelectedNote: (state, action) => {
