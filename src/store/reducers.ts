36e3d4d2160f1245ae97fe6f107007f25de740e0
SmilingWitch <danielitayero@gmail.com>
2024-09-16 16:43:58 -0300

    ref: fix NoteContent

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -117,10 +117,15 @@ const userSlice = createSlice({
       
       const isEmpty = (obj: any) => Object.keys(obj).length === 0; // Función para comprobar si un objeto está vacío
   
-      console.log("NOTE STYLES",noteStyles[noteId].bold)
-      
 
-      if (isEmpty(noteStyles)) {
+
+  console.log("NOTE STYLES", noteStyles[noteId]?.bold);
+  let existingRange = true;
+
+  
+
+
+      /*if (isEmpty(noteStyles)) {
          // Insertamos el objeto con el nuevo rango
          state.noteStyles = newRangeObject;
       } else {
@@ -128,11 +133,17 @@ const userSlice = createSlice({
         if (noteStyles[noteId] && noteStyles[noteId][style]) {
 
           let newRanges = []; // Array para almacenar los nuevos rangos
+         
 
           noteStyles[noteId][style].forEach((range: any) => {
 
+                console.log("selected.start", selected.start);
+                console.log("range.start", range.start);
+                console.log("selected.end", selected.end - 1)
+                console.log("range.end", range.end);
+
             // Si el rango seleccionado esta contenido dentro de algun rango
-            if(range.start <= selected.start && range.end >= selected.end){
+            if(range.start < selected.start && range.end > selected.end - 1){
               console.log("El rango esta contenido dentro de otro rango")  
               newRanges.push({ start: range.start , end: selected.start - 1 }); // Rango anterior
               newRanges.push({ start: selected.end , end: range.end  }); // Rango posterior
@@ -158,12 +169,8 @@ const userSlice = createSlice({
 
               
 
-            } 
-          
-           
-
-            // si el rango existe
-       else if (range.start === selected.start && range.end === selected.end ) {
+            } // si el rango exist. el problema esta en que no entra aqui
+            else if (range.start === selected.start && range.end === selected.end - 1 ) {
         console.log("El rango existe ")
         
 
@@ -175,8 +182,21 @@ const userSlice = createSlice({
         console.log("NOTEEEEEE", noteStyles[noteId][style])
         
         
-      }else if (selected.start > range.start && selected.end - 1 < range.end) {
-        console.log("Hay solapamiento")
+      }else if (selected.start <= range.end && selected.end >= range.start) {
+        console.log("HAY SOLAPAMIENTO")
+        // Si hay solapamiento, pero no está contenido
+        if (selected.start <= range.start) {
+          // Modificamos el rango existente
+          console.log("HAY SOLAPAMIENTO PERO NO ESTA CONTENIDO")
+          newRanges.push({ start: selected.end + 1, end: range.end });
+        } else if (selected.end >= range.end) {
+          console.log("HAY SOLAPAMIENTO PERO Y ESTA CONTENIDO")
+          newRanges.push({ start: range.start, end: selected.start - 1 });
+        } else {
+          // Si el rango no se solapa completamente
+          console.log("EL RANGO NO SE SOLAPAmiento")
+          newRanges.push(range);
+        }
 
                 
       } else {
@@ -187,101 +207,40 @@ const userSlice = createSlice({
           rangeAdd.push({end: selected.end - 1 , start: selected.start })
           console.log("Se agrego un rango",rangeAdd)
 
+          existingRange = false
+        }
+          })
+
+        } else {
+          // Si el estilo no existe aún, creamos el array con el nuevo rango
+          noteStyles[noteId] = {
+            ...noteStyles[noteId],
+            [style]: [{ start: selected.start, end: selected.end }],
+          };
+          console.log('Nuevo estilo creado con el rango', noteStyles[noteId][style]);
+        }
+
+        if(!existingRange){
+          console.log("AGREGAR RANGO")
           noteStyles[noteId][style].push({end: selected.end - 1 , start: selected.start });
           console.log("NOTE STYLES MAS AGREGADO",noteStyles[noteId][style])
 
-          const noteStylesCopy = [...noteStyles[noteId][style]];
+       
+          existingRange = true
+        }
+
+        const noteStylesCopy = [...noteStyles[noteId][style]];
 
           console.log("NOTE STYLES COPIA", noteStylesCopy)
 
-          // Ordena la copia
+          
           const noteSort = noteStylesCopy.sort((a, b) => a.start - b.start);
           console.log("NOTE SORT", noteStyles[noteId][style]);
         
           // Si necesitas actualizar el estado con el array ordenado, asigna la copia ordenada al estado
           state.noteStyles[noteId][style] = noteSort;
-        }
-          })
-        } else {
-          // Si el estilo no existe aún, creamos el array con el nuevo rango
-          noteStyles[noteId] = {
-            ...noteStyles[noteId],
-            [style]: [{ start: selected.start, end: selected.end }],
-          };
-          console.log('Nuevo estilo creado con el rango', noteStyles[noteId][style]);
-        }
       
           
-      }
-
-      
-      
-
-
-
-      /*if (isEmpty(noteStyles)) {
-        // Insertamos el objeto con el nuevo rango
-        state.noteStyles = newRangeObject;
-      } else {
-        console.log("RANGES OFICIAL",noteStyles[noteId][style])
-        // Si ya hay estilos para la nota, verificar si el estilo actual existe (e.g., 'bold')
-        if (noteStyles[noteId] && noteStyles[noteId][style]) {
-          
-          let newRanges = []; // Array para almacenar los nuevos rangos
-          noteStyles[noteId][style].forEach((range: any) => {
-
-
-            // Si el rango seleccionado es exactamente igual al rango existente
-            if (range.start === selected.start && range.end === selected.end - 1) {
-              noteStyles[noteId][style] = noteStyles[noteId][style].filter((range: { start: number; end: number }) => {
-                return !(range.start === selected.start && range.end === selected.end - 1);
-              });              
-              
-            }
-      
-            // Si el rango seleccionado está dentro del rango existente
-            if (selected.start > range.start && selected.end - 1 < range.end) {
-              // Dividimos el rango en dos
-              console.log("El rango seleccionado está dentro del rango existente")
-              newRanges.push({ start: range.start, end: selected.start - 1 }); // Rango anterior
-              newRanges.push({ start: selected.end + 1, end: range.end }); // Rango posterior
-
-              console.log("range.start", range.start)
-              console.log("selected.start - 1", selected.start - 1)
-              console.log("selected.end + 1", selected.end + 1)
-              console.log("end: range.end", range.end)
-              console.log(newRanges)
-            } else if (selected.start <= range.end && selected.end >= range.start) {
-              // Si hay solapamiento, pero no está contenido
-              if (selected.start <= range.start) {
-                // Modificamos el rango existente
-                newRanges.push({ start: selected.end + 1, end: range.end });
-              } else if (selected.end >= range.end) {
-                newRanges.push({ start: range.start, end: selected.start - 1 });
-              } else {
-                // Si el rango no se solapa completamente
-                newRanges.push(range);
-              }
-            }else {
-              // Si no hay solapamiento, simplemente añade el rango existente
-              console.log("NO hay solapamiento", selected)
-              console.log("RANGEEEEEEEEEEEE", range)
-              noteStyles[noteId][style].push({end: selected.end - 1, start: selected.start });
-
-              console.log(noteStyles[noteId][style])
-            }
-          });
-      
-          // Actualizamos los rangos para el estilo
-        
-        } else {
-          // Si el estilo no existe aún, creamos el array con el nuevo rango
-          noteStyles[noteId] = {
-            ...noteStyles[noteId],
-            [style]: [{ start: selected.start, end: selected.end }],
-          };
-          console.log('Nuevo estilo creado con el rango', noteStyles[noteId][style]);
-        }
       }*/
       console.log("--------------------------------------------------------------------------")
     },
