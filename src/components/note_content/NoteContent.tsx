1e89162ad563cbf837a0b38d63583c9506a6697e
SmilingWitch <danielitayero@gmail.com>
2024-09-16 20:57:43 -0300

    ref: comment FormattingBar in NoteContent

--- src/components/note_content/NoteContent.tsx
+++ src/components/note_content/NoteContent.tsx
@@ -12,6 +12,9 @@ import StyledText from "../common/StyledText"
 
 const NoteContent = ({route, setInput}: NoteContentProps) => {
       const {content} = route.params
+      const {id} = route.params
+
+      console.log(content)
       const inputRef = useRef(null);
       const handleFocus = () => {
             inputRef.current?.focus(); // Muestra el teclado
@@ -23,6 +26,7 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
         const [selection, setSelection] = useState({ start: 0, end: 0 });
         const [formattedText, setFormattedText] = useState("")
         
+
           const handleTextChange = (text: string) => {
             setInput(text); // Actualiza el primer estado
             setinput(text); // Actualiza el segundo estado
@@ -30,34 +34,33 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
         const dispatch = useDispatch()
         const noteStyles = useSelector((state: any) => state.noteStyles)
 
-        console.log("ARRAY",noteStyles.note_id.bold)
-        
-        /*console.log("NOTE STYLES", noteStyles.note_id.bold)*/
 
         useEffect(() => {
-          if(noteStyles.note_id){
-            const format = applyInitialStyles(noteStyles,content)
+          if(noteStyles[id]){
+            const format = applyInitialStyles(noteStyles,content, id)
             setFormattedText(format)
+          } else {
+            setFormattedText(content)
           }
           
         }, [noteStyles])
 
-        
-
        
 
+        console.log("noteStyles",noteStyles)
 
-        const createRange =  (selected: any, style: string) =>{
+        const createRange =  (selected: any, style: string, note_id: number) =>{
+
           if(selected.start !== selected.end){
-            dispatch(addNotesStyle({style: style, selected: selected, noteStyles: noteStyles}))
-            const format = applyStyles(noteStyles, content, noteStyles.note_id.bold[0])
+            dispatch(addNotesStyle({style: style, selected: selected, noteStyles: noteStyles, note_id: note_id}))
+            const format = applyStyles(noteStyles, content, noteStyles[note_id].bold[0], note_id)
             setFormattedText(format)
           }
         }
 
         const getSelectedText = (text: string, selection: { start: number; end: number }) => {
 
-          console.log("SELECTION", selection )
+          /*console.log("SELECTION", selection )*/
           const beforeText = text.substring(0, selection.start);
           const selectedText = text.substring(selection.start, selection.end);
           const afterText = text.substring(selection.end);
@@ -65,18 +68,11 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
           return {beforeText, selectedText, afterText}
       };
 
-        const applyStyles = (noteStyles: any, text: any, selected: any) => {
+        const applyStyles = (noteStyles: any, text: any, selected: any, note_id: number) => {
 
-
-          const {beforeText,selectedText, afterText} = getSelectedText(text, selected)
-
-          console.log("beforeText", beforeText)
-          console.log("selectedText", selectedText)
-          console.log("afterText", afterText)
-
-          console.log("RANGE",noteStyles.note_id.bold)
           
-            if(noteStyles.note_id.bold){
+          const {beforeText,selectedText, afterText} = getSelectedText(text, selected)          
+            if(noteStyles[note_id].bold){
               return(<StyledText>
                       <StyledText >{beforeText}</StyledText>
                       <StyledText  fontWeight="bold">{selectedText}</StyledText>  
@@ -90,29 +86,30 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
 
 
 
-        const applyInitialStyles = (noteStyles: any, text: string) => {
+        const applyInitialStyles = (noteStyles: any, text: string, note_id: number) => {
           let styledTextComponents: JSX.Element[] = [];
           let currentIndex = 0; // Para rastrear la posición actual del texto
         
           // Verifica si hay estilos aplicados en la nota
-          if (noteStyles.note_id) {
+          if (noteStyles[note_id]) {
             // Recorre cada estilo (por ejemplo: 'bold', 'italic', etc.)
-            Object.keys(noteStyles.note_id).forEach((style: string) => {
+            Object.keys(noteStyles[note_id]).forEach((style: string) => {
               // Recorre cada rango para el estilo
-              noteStyles.note_id[style].forEach((range: { start: number; end: number }) => {
+              noteStyles[note_id][style].forEach((range: { start: number; end: number }) => {
                 // Extraer el texto antes del rango
                 if (currentIndex < range.start) {
                   console.log("currentIndex",currentIndex)
-                  console.log("range.start",range.start)
+                  console.log("range.start",range.start - 1)
                   styledTextComponents.push(
-                    <StyledText key={`text-${currentIndex}`}>{text.substring(currentIndex, range.start - 1)}</StyledText>
+                    //range.start
+                    <StyledText key={`text-${currentIndex}`}>{text.substring(currentIndex, range.start - 1 )}</StyledText>
                   );
                 }
         
                 // Extraer el texto dentro del rango y aplicar el estilo
                 styledTextComponents.push(
                   <StyledText key={`styled-${range.start}`} fontWeight={style === 'bold' ? 'bold' : undefined}>
-                    {text.substring(range.start - 1 , range.end)}
+                    {text.substring(range.start -1  , range.end)}
                   </StyledText>
                 );
         
@@ -128,6 +125,7 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
               );
             }
           } else {
+            /*console.log("TEXTO ORIGINAL")*/
             // Si no hay estilos aplicados, devolver el texto original
             styledTextComponents.push(<StyledText key="plain-text">{text}</StyledText>);
           }
@@ -162,33 +160,33 @@ const NoteContent = ({route, setInput}: NoteContentProps) => {
                 
 
 
-        <View>
+        {/*<View>
               <ScrollView horizontal style = {styles.scroll_container}  keyboardShouldPersistTaps="always">
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'bold' onPress={() => {createRange(selection, "bold")
+                  <FormatingButton name = 'bold' onPress={() => {createRange(selection, "bold", id)
 
                   }}/>
-                  <FormatingButton name = 'italic' onPress={() => createRange(selection, "italic")}/>
-                  <FormatingButton name = 'circle' /*onPress={() => handleFormat('resalt')}*//>
-                  <FormatingButton name = 'text-height' /*onPress={() => handleFormat('height')}*//>
+                  <FormatingButton name = 'italic' onPress={() => createRange(selection, "italic", id)}/>
+                  <FormatingButton name = 'circle' />
+                  <FormatingButton name = 'text-height' />
               </View>
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'check-square-o' /*onPress={() => handleFormat('to-do-list')}*/> </FormatingButton>
-                  <FormatingButton name = 'list-ul' /*onPress={() => handleFormat('list-binetas')}*//>
-                  <FormatingButton name = 'list-ol' /*onPress={() => handleFormat('enum-list')}*//>
+                  <FormatingButton name = 'check-square-o' > </FormatingButton>
+                  <FormatingButton name = 'list-ul' />
+                  <FormatingButton name = 'list-ol' />
 
               </View>
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'align-justify' /*onPress={() => handleFormat('justify')}*//>
-                  <FormatingButton name = 'align-left' /*onPress={() => handleFormat('aling-left')}*//>
-                  <FormatingButton name = 'align-right' /*onPress={() => handleFormat('aling-right')}*//>
+                  <FormatingButton name = 'align-justify' />
+                  <FormatingButton name = 'align-left' />
+                  <FormatingButton name = 'align-right' />
               </View>
               <View style = {styles.icon_bx}>
-                  <FormatingButton name = 'rotate-right' /*onPress={() => handleFormat('back')}*//>
-                  <FormatingButton name = 'rotate-left' /*onPress={() => handleFormat('behind')}*//>
+                  <FormatingButton name = 'rotate-right' />
+                  <FormatingButton name = 'rotate-left' />
               </View>
           </ScrollView>
-          </View>
+          </View>*/}
             </KeyboardAvoidingView>
 
     )
