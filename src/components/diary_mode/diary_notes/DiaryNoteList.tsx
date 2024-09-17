7be137b6c5e2d2dff574d864b43ac88e9c20af28
SmilingWitch <danielitayero@gmail.com>
2024-09-16 21:04:58 -0300

    feat: add DiaryNoteList.tsx

---
+++ src/components/diary_mode/diary_notes/DiaryNoteList.tsx
@@ -0,0 +1,61 @@
+
+
+import { FlatList, StyleSheet, View } from "react-native"
+import { useEffect, useState } from "react";
+import DiaryNoteItem from "./DiaryNoteItem";
+import { NotesListProps } from "../../../types";
+import lighTeme from "../../../lightTheme";
+import StyledText from "../../common/StyledText";
+
+
+
+const DiaryNotesList = () => {
+
+    const data = [
+        {id: 1},
+        {id: 2}
+    ]
+
+    const renderItem = ({item}: any) => (
+        <DiaryNoteItem />   
+    )
+
+    return(
+        <FlatList
+            data = {data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+
+            ListEmptyComponent={
+                <View style = {styles.empty_list}>
+                    <StyledText>
+                        No notes
+                    </StyledText>
+                    <StyledText fontSize="small" >
+                        Tab the Add button to create a note
+                    </StyledText>
+                </View> 
+            }
+        ></FlatList>
+    )
+}
+
+const styles = StyleSheet.create({
+    containerWrapp:{
+       justifyContent: 'space-between',
+       paddingHorizontal: lighTeme.padding,
+
+    },
+    container: {
+        backgroundColor: lighTeme.colors.primary,
+    },
+    empty_list: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '60%'
+    }
+
+})
+
+export default DiaryNotesList
