96ecd024669dc63d177b26e732ab38683c55a7b5
SmilingWitch <danielitayero@gmail.com>
2024-09-19 19:29:54 -0300

    feat: add TrouncatedText to DiaryNoteItem.tsx

--- src/components/diary_mode/diary_notes/DiaryNoteItem.tsx
+++ src/components/diary_mode/diary_notes/DiaryNoteItem.tsx
@@ -1,16 +1,34 @@
 import { View, StyleSheet,TouchableOpacity } from "react-native"
 import StyledText from "../../common/StyledText"
 import lighTeme from "../../../lightTheme"
+import formatedDataWithString from "../../../functions/formatedDataWithString"
+import TruncatedText from "../../common/TruncatedText"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ParamListBase } from "@react-navigation/native"
+
+interface DiaryNoteItemProp{
+    item: any,
+    navigation:NativeStackNavigationProp<ParamListBase>
+}
+
+const DiaryNoteItem = ({item, navigation}: DiaryNoteItemProp) => {
+
+    
+    const {dayOfWeek, month, year, dayOfMonth } = formatedDataWithString(item.date)
 
 
-const DiaryNoteItem = () => {
     return(
-        <TouchableOpacity style = {styles.container}>
+        <TouchableOpacity style = {styles.container} onPress = {() => navigation.navigate('DiaryNote', 
+                                                    {title : item.title, 
+                                                    content: item.content, 
+                                                    new_note : false,
+                                                    date : item.date})}>
             <View style = {styles.date}>
-                <StyledText fontSize="h1" fontWeight="bold">16</StyledText>
-                <StyledText fontSize="small">Sep</StyledText>
+                <StyledText fontSize="h1" fontWeight="bold">{dayOfMonth}</StyledText>
+                <StyledText fontSize="small">{month}, {year}</StyledText>
             </View>
-            <StyledText fontSize="small"> Holaaaaaaa</StyledText>
+            {item.title && <TruncatedText text={item.title} maxLength={100}/>}
+            {item.content && <TruncatedText text={item.content} maxLength={100} fontSize = "small"/> }
         </TouchableOpacity>
     )
 }
@@ -22,12 +40,15 @@ const styles = StyleSheet.create({
         backgroundColor: lighTeme.colors.grey,
         margin: 5,
         width: '90%',
-        alignSelf: 'center'
+        alignSelf: 'center',
+        justifyContent: 'center',
+        paddingBottom: 20
+
     },
     date: {
         flexDirection: 'row',
         alignItems: 'center',
-        gap: 5
+        gap: 5,
     }
 })
 
